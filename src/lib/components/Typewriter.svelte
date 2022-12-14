d2accf0797f8774bdcfc167101906cb717e5ffed
Ken Jones <kenjwho@gmail.com>
2022-12-13 20:24:20 -0600

    Add Hero button. Add header resume link. Start menu fix

--- src/lib/components/Typewriter.svelte
+++ src/lib/components/Typewriter.svelte
@@ -2,6 +2,7 @@
 	import { onMount, tick } from 'svelte';
 	import { data } from '$lib/data';
 	import { browser } from '$app/environment';
+	import { scrollToElementById } from '$lib/utils';
 
 	let text = data.titles[0];
 	let i = 1;
@@ -50,6 +51,14 @@
 <div class="relative">
 	<div
 		use:swipittySwap={text}
-		class={`transition mt-4 duration-1000 absolute top-0 left-0  text-gray text-7xl sm:text-[80px] md:text-[110px] lg:text-[140px] xl:text-[160px]  2xl:text-[180px] font-serif font-bold tracking-[-.06em]`}
+		class={`relative transition mt-4 duration-1000 absolute top-0 left-0  text-gray text-7xl sm:text-[80px] md:text-[110px] lg:text-[140px] xl:text-[160px]  2xl:text-[180px] font-serif font-bold tracking-[-.06em]`}
 	/>
+
+	<button
+		on:click={() => {
+			scrollToElementById('work');
+		}}
+		class="absolute bottom-[-80px] w-auto justify-center rounded-full border border-gray hover:border-gray bg-gray hover:bg-white inline-flex gap-[22px] px-[30px] py-[17px] text-white hover:text-gray font-medium text-xl items-center transition"
+		>See Work</button
+	>
 </div>
