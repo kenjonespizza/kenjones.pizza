1ec68d5ac1bafa33b06e1066ba61098f1410de89
Ken Jones <kenjwho@gmail.com>
2023-01-05 23:57:00 -0600

    Fix fix redirect?

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-	goto(data.links.resume);
+	onMount(() => {
+		// Redirect to the resume
+		goto(data.links.resume);
+	});
 </script>
